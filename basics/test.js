023557ba652b5ba5472f002d3db68dcbb974360b
sonuthecoder96 <128586900+sonuthecoder96@users.noreply.github.com>
2025-05-27 10:30:42 +0000

    datatypes

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,18 @@
 console.log("hello")
+const accountId = 14453
+let accountemail = "abc@gmail.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2; //not allowed
+
+accountemail = "sjsj"
+accountpassword = "999"
+accountCity =  "delhi"
+
+console.table([accountId,accountemail,accountpassword])
+/*
+Prefered not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
---
+++ basics/test2.js
@@ -0,0 +1,22 @@
+"use strict"; //treate all Js code as Newer Version
+
+//alert(3+3) //we are using nodejs, not browser
+
+let name = "abc"
+let age = 18
+let islogged  = false
+
+// Primitive datatypes
+// number => 2 to power 53
+// bigint
+// boolean =>true/false
+// null => standalonse value khali value
+// undefined => value not assigned
+// symbol => uniquness
+
+
+// object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
