18adfa0ef543ad9429fc901e1c6c73fde7a65468
sonuthecoder96 <128586900+sonuthecoder96@users.noreply.github.com>
2025-05-28 11:39:23 +0000

    Math and Numbers

---
+++ basics/string.js
@@ -0,0 +1,41 @@
+const name = "ani"
+
+
+console.log(`hello my name is ${name}`)//modern way
+
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+console.log(name.indexOf('a'));
+
+
+const newstring = name.substring(0,2);
+
+const temp = "abcdefghijkl"
+
+console.log(temp.substring(2,3))
+
+console.log(temp.slice(2,3))
+
+console.log(newstring)
+
+const s1 = "     name   ";
+
+
+
+console.log(s1.trim())
+
+
+console.log(s1.includes("n"))
+
+
+console.log(temp.replace("ab","12"));
+
+
+const s2 = "hello world my name"
+
+console.log(s2.split(' '));//return array on basis of split on given char
+
+
+
+
+
