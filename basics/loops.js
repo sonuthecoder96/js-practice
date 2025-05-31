29decb5d0a78de561568ca6a2e8d4285057dbabd
sonuthecoder96 <128586900+sonuthecoder96@users.noreply.github.com>
2025-05-31 12:04:32 +0000

    loops

---
+++ basics/loops.js
@@ -0,0 +1,30 @@
+//for 
+for (let index = 0; index < 10; index++) {
+    //console.log(index)
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i*j}`);
+    
+     }
+     
+    
+}
+
+//while loop
+let constanst =   0
+while(constanst<10){
+    console.log("while")
+    constanst++;
+
+}
+constanst = 0;
+//do while in it in all cases it occurs atleats one time
+do{
+    console.log(" do while")
+    constanst+=4;
+
+
+}while(constanst<20);
