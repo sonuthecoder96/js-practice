ad98d938ca4c30b526e3f4b01b64cc54eb105f4b
sonuthecoder96 <128586900+sonuthecoder96@users.noreply.github.com>
2025-05-31 18:47:44 +0000

    otherloops

---
+++ basics/otherloops.js
@@ -0,0 +1,31 @@
+const arr = [1,2,3,4,5]
+//FOR OFF LOOP
+for (const iterator of arr) {
+    console.log(iterator)
+    
+}
+const greetings = "hello wrold!"
+for(const i of greetings){
+    console.log(`each char is ${i}`);
+}
+
+//MAPS
+
+const map = new Map()
+map.set('In','India')
+map.set("usa","america")
+map.set('In','India')//not get inserted as already existed
+//only contain unique values
+for (const [key,value] of map) {
+    console.log(key,':-',value);
+    
+}
+
+const myobject = {
+    'game1':'gta6'
+}
+//object is not iteratble by foroff loop
+for (const i of myobject) {
+
+    
+}
